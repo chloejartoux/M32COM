f9a57aaf9f2cba65cd13c37fe47c90093870218a
Sh0ri <quent.dasilva@gmail.com>
2017-10-12 02:18:45 +0100

    Initialisation

---
+++ M32COM/Models/EmployeeModels/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M32COM.Models.EmployeeModels
+{
+    public class Employee
+    {
+        public string Code { get; set; }
+    }
+}
---
+++ M32COM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace M32COM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
